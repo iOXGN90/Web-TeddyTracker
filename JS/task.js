48983d37b32c67cf2f569172ed9a29a1f22a1b51
saisiah <castulo.kirk@gmail.com>
2024-04-24 01:07:48 +0800

    i

--- JS/task.js
+++ JS/task.js
@@ -11,74 +11,74 @@ toggle.addEventListener("click", () => {
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
 
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light Mode"
-    }else{
-        modeText.innerText = "Dark Mode"
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
     }
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
     var dragSrcEl = null;
-    
+
     function handleDragStart(e) {
-      this.style.opacity = '0.1';
-      this.style.border = '3px dashed #c4cad3';
-      
-      dragSrcEl = this;
-  
-      e.dataTransfer.effectAllowed = 'move';
-      e.dataTransfer.setData('text/html', this.innerHTML);
+        this.style.opacity = '0.1';
+        this.style.border = '3px dashed #c4cad3';
+
+        dragSrcEl = this;
+
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
     }
-    
+
     function handleDragOver(e) {
-      if (e.preventDefault) {
-        e.preventDefault();
-      }
-  
-      e.dataTransfer.dropEffect = 'move';
-      
-      return false;
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+
+        e.dataTransfer.dropEffect = 'move';
+
+        return false;
     }
-  
+
     function handleDragEnter(e) {
-      this.classList.add('task-hover');
+        this.classList.add('task-hover');
     }
-  
+
     function handleDragLeave(e) {
-      this.classList.remove('task-hover');
+        this.classList.remove('task-hover');
     }
-  
+
     function handleDrop(e) {
-      if (e.stopPropagation) {
-        e.stopPropagation(); // stops the browser from redirecting.
-      }
-      
-      if (dragSrcEl != this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
-      }
-      
-      return false;
+        if (e.stopPropagation) {
+            e.stopPropagation(); // stops the browser from redirecting.
+        }
+
+        if (dragSrcEl != this) {
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+
+        return false;
     }
-  
+
     function handleDragEnd(e) {
-      this.style.opacity = '1';
-      this.style.border = 0;
-      
-      items.forEach(function (item) {
-        item.classList.remove('task-hover');
-      });
+        this.style.opacity = '1';
+        this.style.border = 0;
+
+        items.forEach(function (item) {
+            item.classList.remove('task-hover');
+        });
     }
-    
-    let items = document.querySelectorAll('.task'); 
-    items.forEach(function(item) {
-      item.addEventListener('dragstart', handleDragStart, false);
-      item.addEventListener('dragenter', handleDragEnter, false);
-      item.addEventListener('dragover', handleDragOver, false);
-      item.addEventListener('dragleave', handleDragLeave, false);
-      item.addEventListener('drop', handleDrop, false);
-      item.addEventListener('dragend', handleDragEnd, false);
+
+    let items = document.querySelectorAll('.task');
+    items.forEach(function (item) {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
     });
 });
 
@@ -90,8 +90,8 @@ function toggleDropdown(event) {
     dropdownContent.style.left = event.target.offsetLeft + "px";
     dropdownContent.style.top = (event.target.offsetTop + event.target.offsetHeight) + "px";
 }
-  
-window.onclick = function(event) {
+
+window.onclick = function (event) {
     if (!event.target.matches('.bx-dots-horizontal-rounded')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         for (var i = 0; i < dropdowns.length; i++) {
@@ -108,3 +108,21 @@ dropdownButtons.forEach(button => {
     button.addEventListener('click', toggleDropdown);
 });
 
+// code.html
+
+Swal.fire({
+  title: "Enter Code",
+  html:
+    '<input id="swal-input1" class="swal2-input" placeholder="Enter Code">',
+  focusConfirm: false,
+  preConfirm: () => {
+    return document.getElementById("swal-input1").value;
+  }
+})
+  .then((result) => {
+    if (result.isConfirmed) {
+      // Handle the entered code here
+      const enteredCode = result.value;
+      console.log("Entered code:", enteredCode);
+    }
+  });
